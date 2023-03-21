dd628d44b3f96170e3d73506f4c433a3f6e20868
PC1-IFLAB <>
2023-03-21 16:55:08 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using modul5_1302210055;
+
+Console.WriteLine(Penjumlahan.JumlahTigaAngka<double>(13.0, 02.0, 21.0));
+
---
+++ SimpelDataBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302210055
+{
+    internal class SimpelDataBase
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public void SimpelDataBase()
+        {
+            storedData = new List<T>;
+            inputDates = new List<DateTime>();
+        }
+
+
+    }
+}
